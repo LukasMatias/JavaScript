e00b6ddd83231966905ab4bf31539963e002c26f
Lukas Matias <lukaskt6@gmail.com>
2020-07-15 01:02:54 -0300

    segundo

--- public_html/js/stringsComCitacoes.js
+++ public_html/js/stringsComCitacoes.js
@@ -3,8 +3,7 @@ var msg;
 title = "Olá veja as nossas ofertas especiais";
 msg = '<a href=\"sale.html\">25% de desconto!</a>';
 
-var elTitle = document.getElementById('title');
-elTitle.innerHTML = title;
+var elTitle = document.getElementById('title').innerHTML = title;
+
 var elNote = document.getElementById('note');
 elNote.innerHTML = msg;
-msg = elNote;
